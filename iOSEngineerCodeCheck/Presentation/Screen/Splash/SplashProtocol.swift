4ea11a96073584e6966e9bf0adc91a45e5b5b798
kntkymt <kntkc0de@gmail.com>
2021-10-24 04:31:24 +0900

    [Fix] SplashViewControllerをMVPにする

---
+++ iOSEngineerCodeCheck/Presentation/Screen/Splash/SplashProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  SplashViewProtocol.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by kntk on 2021/10/24.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+
+protocol SplashView: AnyObject {
+    func routeToMain()
+}
+
+protocol SplashPresentation: Presentation {
+}
