55a958fa6ac0e070170f07d060683c6463365863
kntkymt <kntkc0de@gmail.com>
2021-10-24 02:09:17 +0900

    [Add] リポジトリ検索API仕様定義

---
+++ iOSEngineerCodeCheck/Network/Target/SearchTarget.swift
@@ -0,0 +1,38 @@
+//
+//  SearchTarget.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by kntk on 2021/10/24.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import Moya
+
+struct SearchRepositoryResponse: Codable {
+    var items: [Repository]
+}
+
+struct SearchRepositoryRequest {
+    var query: String
+}
+
+extension SearchRepositoryRequest: APITargetType {
+
+    typealias Response = SearchRepositoryResponse
+
+    var path: String {
+        return "/search/repositories"
+    }
+
+    var method: Method {
+        return .get
+    }
+
+    var task: Task {
+        let parameters: Parameters = [
+            "q": query
+        ]
+
+        return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+    }
+}
