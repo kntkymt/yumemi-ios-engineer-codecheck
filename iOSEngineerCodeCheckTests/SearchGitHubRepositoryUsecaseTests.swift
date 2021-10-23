6294aac93966102fb0980a182644092943c369e8
kntkymt <kntkc0de@gmail.com>
2021-10-24 03:02:34 +0900

    [Add] 簡単なテストでUsecaseが正常に動作することを確認

---
+++ iOSEngineerCodeCheckTests/SearchGitHubRepositoryUsecaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  SearchGitHubRepositoryUsecaseTests.swift
+//  iOSEngineerCodeCheckTests
+//
+//  Created by kntk on 2021/10/24.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+
+import XCTest
+@testable import iOSEngineerCodeCheck
+
+final class SearchGitHubRepositoryUsecaseTests: XCTestCase {
+
+    // MARK: - Property
+
+    private let usecase: SearchGitHubReposiotyUsecase = SearchGitHubRepositoryUsecaseImpl(gitHubRepositoryRepository: GitHubRepositoryRepositoryImpl())
+
+    override class func setUp() {
+        super.setUp()
+
+        API.setup(provider: APIProviderFactory.createService())
+    }
+
+    func testGetGitHubRepository() {
+        Task {
+            do {
+                let repositories = try await usecase.searchGitHubRepositories(by: "kntkymt")
+
+                XCTAssertFalse(repositories.isEmpty)
+            } catch {
+                XCTAssertThrowsError(error)
+            }
+        }
+    }
+}
