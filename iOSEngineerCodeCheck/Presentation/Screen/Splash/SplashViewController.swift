2bcb6e81158dffd7b87e3d5737b6d8fb206be694
kntkymt <kntkc0de@gmail.com>
2021-10-24 00:01:05 +0900

    [Add] SplashViewController LaunchScreenをViewControllerにしたもの

---
+++ iOSEngineerCodeCheck/Presentation/Screen/Splash/SplashViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SplashViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by kntk on 2021/10/23.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class SplashViewController: UIViewController, Storyboardable {
+
+    // MARK: - Build
+
+    static func build() -> Self {
+        return initViewController()
+    }
+
+    // viewDidAppearでないと正常に動作しない
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        SceneRouter.shared.route(to: .main, animated: true)
+    }
+}
