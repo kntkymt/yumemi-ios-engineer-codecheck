d44971937e82c67a8e02592418a1373712994ff5
kntkymt <kntkc0de@gmail.com>
2021-10-26 17:48:27 +0900

    [Fix] iPhoneSEだとReadmeの読み込みが遅くWebViewの高さが拡張されないので待つ時間を0.1秒から0.2秒に修正

--- iOSEngineerCodeCheck/Presentation/Screen/GitHubRepositoryDetail/GitHubRepositoryDetailReadMe/GitHubRepositoryDetailReadMeViewController.swift
+++ iOSEngineerCodeCheck/Presentation/Screen/GitHubRepositoryDetail/GitHubRepositoryDetailReadMe/GitHubRepositoryDetailReadMeViewController.swift
@@ -70,7 +70,7 @@ extension GitHubRepositoryDetailReadMeViewController: GitHubRepositoryDetailRead
             }
 
             // js適応からscrollViewのcontentSizeが反映されるまで少し待つ
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
                 self.webViewHeightConstraint.constant = self.webView.scrollView.contentSize.height
             }
         }
