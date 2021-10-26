8a43b49cd688b4d6785e3932b677bee56f232b09
kntkymt <kntkc0de@gmail.com>
2021-10-27 08:30:35 +0900

    [Add] UIViewにUIActivityIndicatorを表示するExtensionを追加

---
+++ iOSEngineerCodeCheck/Common/Extension/UIView+LoadingIndicator.swift
@@ -0,0 +1,65 @@
+//
+//  UIView+LoadingIndicator.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by kntk on 2021/10/27.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+enum IndicatorViewKey {
+    static let tag = 100
+}
+
+extension UIView {
+
+    var indicatorView: UIActivityIndicatorView {
+        get {
+            let indicatorView = UIActivityIndicatorView()
+            indicatorView.style = .large
+            indicatorView.color = .systemGray
+            indicatorView.backgroundColor = .clear
+            indicatorView.frame.size.height = 50
+            indicatorView.tag = IndicatorViewKey.tag
+
+            return indicatorView
+        }
+    }
+
+    func startLoading() {
+        if let addedIndicatorView = getAddedIndicatorView() {
+            addedIndicatorView.isHidden = false
+            isUserInteractionEnabled = false
+            addedIndicatorView.startAnimating()
+        } else {
+            let indicatorView = indicatorView
+            indicatorView.translatesAutoresizingMaskIntoConstraints = false
+            indicatorView.hidesWhenStopped = true
+
+            self.addSubview(indicatorView, constraints: .center())
+
+            isUserInteractionEnabled = false
+            indicatorView.startAnimating()
+        }
+    }
+
+    func stopLoading() {
+        if let addedIndicatorView = getAddedIndicatorView() {
+            isUserInteractionEnabled = true
+            addedIndicatorView.stopAnimating()
+            addedIndicatorView.isHidden = true
+        }
+    }
+
+    private func getAddedIndicatorView() -> UIActivityIndicatorView? {
+        for subview in subviews {
+            if let indicatorView = subview as? UIActivityIndicatorView, indicatorView.tag == IndicatorViewKey.tag {
+                return indicatorView
+            }
+        }
+
+        return nil
+    }
+}
+
