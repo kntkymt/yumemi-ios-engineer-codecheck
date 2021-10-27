438d0d80281047271ab08ecc7ab2cfa5a7ff74d1
kntkymt <kntkc0de@gmail.com>
2021-10-27 19:42:53 +0900

    [Add] MessageBanner WebViewShowableにコメントを追加

--- iOSEngineerCodeCheck/UI/BannerShowable.swift
+++ iOSEngineerCodeCheck/UI/BannerShowable.swift
@@ -7,7 +7,21 @@
 //
 
 protocol BannerShowable {
+
+    /// エラーメッセージをバナーで表示する
+    /// バナーの色は赤色
+    ///
+    /// - parameters:
+    ///     - title: タイトル(1行目に表示)
+    ///     - message: メッセージ(2行目に表示)
     func showErrorBanner(_ title: String, with message: String?)
+
+    /// 警告メッセージをバナーで表示する
+    /// バナーの色は黄色
+    ///
+    /// - parameters:
+    ///     - title: タイトル(1行目に表示)
+    ///     - message: メッセージ(2行目に表示)
     func showWarnBanner(_ title: String, with message: String?)
 }
 
